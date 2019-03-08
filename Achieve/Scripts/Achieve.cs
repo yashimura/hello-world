8e54cad0e1e8f4eeedaad7af97f2fb1f1c989d9e
志村安啓 <yshimura@cosmomachia.co.jp>
2019-03-08 14:36:16 +0900

    20190308

---
+++ Achieve/Scripts/Achieve.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Mix2App.Lib;
+using Mix2App.Lib.Model;
+using Mix2App.Lib.Events;
+using Mix2App.Lib.View;
+using Mix2App.Lib.Utils;
+
+namespace Mix2App.Achieve
+{
+    public class Achieve : MonoBehaviour, IReceiver, IReadyable
+    {
+        [SerializeField] private GameObject Container = null;
+        [SerializeField] private GameObject PrefabList = null;
+        [SerializeField] private GameObject EventRoot = null;
+
+
+        private const int LIST_MAX = 100;
+
+        private List<AchieveData> mData;
+        private GameObject[] prefabObj = new GameObject[LIST_MAX];
+
+        void Awake()
+        {
+        }
+
+        public void receive(params object[] parameter)
+        {
+            GameCall call = new GameCall(CallLabel.GET_ACHIEVES);
+            call.AddListener(mGetAchieves);
+            ManagerObject.instance.connect.send(call);
+
+        }
+        void mGetAchieves(bool success, object data)
+        {
+            Debug.Log(success + "/" + data);
+            if (success)
+            {
+                mData = (List<AchieveData>)data;
+
+                StartCoroutine(mStart());
+            }
+        }
+
+
+
+
+
+        private bool mready = false;
+        public bool ready()
+        {
+            return mready;
+        }
+
+        void OnDestroy()
+        {
+        }
+
+        void Start()
+        {
+        }
+
+        IEnumerator mStart()
+        {
+            EventRoot.transform.Find("Achieve_list/Button_blue_tojiru").gameObject.GetComponent<Button>().onClick.AddListener(ButtonTojiruClick);
+
+            PrefabSet();
+
+            mready = true;
+
+            yield return null;
+        }
+
+        private void PrefabSet()
+        {
+            for (int i = 0; i < mData.Count && i < LIST_MAX; i++)
+            {
+                // プレハブを登録
+                prefabObj[i] = (GameObject)Instantiate(PrefabList);
+                prefabObj[i].transform.SetParent(Container.transform, false);
+                prefabObj[i].name = "AchieveList" + i.ToString();
+
+                // 背景の色を偶数、奇数で変更する
+                prefabObj[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f * (i & 1));
+
+                // アチーブメントのタイトルと回数を登録
+                prefabObj[i].transform.Find("Title").gameObject.GetComponent<Text>().text = mData[i].atitle;
+                prefabObj[i].transform.Find("Times").gameObject.GetComponent<Text>().text = "あと" + mData[i].count.ToString() + "かい";
+            }
+        }
+
+
+        void Update()
+        {
+        }
+
+            private void ButtonTojiruClick()
+        {
+            ManagerObject.instance.sound.playSe(17);
+            ManagerObject.instance.view.change(SceneLabel.HOME);
+        }
+
+
+    }
+}
---
+++ Achieve/Scripts/Achieve_clear.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Mix2App.Lib;
+using Mix2App.Lib.Model;
+using Mix2App.Lib.Events;
+using Mix2App.Lib.View;
+using Mix2App.Lib.Utils;
+
+namespace Mix2App.Achieve
+{
+    public class Achieve_clear : MonoBehaviour, IReceiver, IReadyable
+    {
+        [SerializeField] private GameEventHandler GEHandler = null;
+        [SerializeField] private GameObject CameraObj = null;
+        [SerializeField] private GameObject EventRoot = null;
+
+
+        private AchieveData mData;
+        private object[] mparam;                                            // 他のシーンからくるパラメータ
+
+
+        void Awake()
+        {
+            mparam = null;
+        }
+
+        public void receive(params object[] parameter)
+        {
+            mparam = parameter;
+
+            if (mparam == null)
+            {
+                mparam = new object[] {
+                    new AchieveData(),
+                    4,                                                          // int  カメラDepth値
+                };
+
+            }
+            mData = (AchieveData)mparam[0];
+
+            CameraObj.transform.GetComponent<Camera>().depth = (int)mparam[1];
+
+            StartCoroutine(mStart());
+        }
+
+        private bool mready = false;
+        public bool ready()
+        {
+            return mready;
+        }
+
+        void OnDestroy()
+        {
+        }
+
+        void Start()
+        {
+        }
+
+        IEnumerator mStart()
+        {
+            RewardBehaviour _rb = EventRoot.transform.Find("clear_dialog/RewardView").gameObject.GetComponent<RewardBehaviour>();
+            _rb.init(mData.reward);                                             // 報酬セット
+
+            EventRoot.transform.Find("clear_dialog/Button_blue_tojiru").gameObject.GetComponent<Button>().onClick.AddListener(ButtonTojiruClick);
+
+            mready = true;
+
+            yield return null;
+        }
+
+        void Update()
+        {
+        }
+
+        private void ButtonTojiruClick()
+        {
+            ManagerObject.instance.sound.playSe(17);
+            GEHandler.OnRemoveScene(SceneLabel.ACHIEVE_CLEAR);
+        }
+
+
+
+    }
+}
