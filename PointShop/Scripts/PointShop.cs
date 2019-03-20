34b8cd9f345430f137f1cbcdf7c13d83830ba5ff
志村安啓 <yshimura@cosmomachia.co.jp>
2019-03-20 19:35:25 +0900

    20190320-2

--- PointShop/Scripts/PointShop.cs
+++ PointShop/Scripts/PointShop.cs
@@ -24,12 +24,57 @@ namespace Mix2App.PointShop
         [SerializeField] private Sprite[] ApplitchiImage = null;        // アプリっちの表示スプライトリスト
         [SerializeField] private GameObject PrefabItem = null;
 
+
+        private const int ITEM_MAX = 100;
+
+        private object[] mparam;
+        private GameObject[] prefabObj = new GameObject[ITEM_MAX];
+
+
+
+        private List<ItemData> items;
+
+
         void Awake()
         {
+            mparam = null;
+            mready = false;
         }
 
         public void receive(params object[] parameter)
         {
+            mparam = parameter;
+
+            if (mparam == null)
+            {
+            }
+
+            items = new List<ItemData>();
+
+            ItemData item = new ItemData();
+            item.code = "tg18_as16058_1";
+            item.title = "テストアイテム";
+            item.kind = 0;
+            item.version = "tg18";
+            items.Add(item);
+
+            item = new ItemData();
+            item.code = "tg18_as16055_1";
+            item.title = "テストアイテム";
+            item.kind = 0;
+            item.version = "tg18";
+            items.Add(item);
+
+            item = new ItemData();
+            item.code = "tg18_as16053_1";
+            item.title = "テストアイテム";
+            item.kind = 0;
+            item.version = "tg18";
+            items.Add(item);
+
+            StartCoroutine(prefabItemSet());
+
+
         }
 
         private bool mready = false;
@@ -49,6 +94,38 @@ namespace Mix2App.PointShop
         void Update()
         {
         }
+
+        private IEnumerator prefabItemSet()
+        {
+
+            ItemDataSet();
+
+            yield return null;
+        }
+
+        private void ItemDataSet()
+        {
+            for (int i = 0; i < items.Count && i < ITEM_MAX; i++)
+            {
+                // プレハブを登録
+                prefabObj[i] = (GameObject)Instantiate(PrefabItem);
+                prefabObj[i].transform.SetParent(ItemContainer.transform, false);
+                prefabObj[i].name = "Item" + i.ToString();
+
+                // アイテムを表示
+                ItemBehaviour ibItem;
+
+                ibItem = prefabObj[i].transform.Find("Button_item/ItemView").gameObject.GetComponent<ItemBehaviour>();
+                ibItem.init(items[i]);
+
+                ibItem = prefabObj[i].transform.Find("Item_selected/ItemView").gameObject.GetComponent<ItemBehaviour>();
+                ibItem.init(items[i]);
+
+            }
+
+        }
+
+
 
 
 
